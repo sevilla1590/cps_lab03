d0f30bf54f43b50ab7fa763345bb0b5bce6b72fc
sevilla1590 <sevillaizan@gmail.com>
2026-05-16 01:46:58 -0500

    commit ejercicio adic

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -1,4 +1,9 @@
 package pe.edu.tecsup.lab03.controllers;
 
 public class StudentController {
+    public StudentController(int id, String nombre, String apellido, double promedio) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.promedio = promedio; }
 }
