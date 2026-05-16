b3974e68a5346ca6b4bf2d3ea4d277e5aa404a2a
Emilio2511 <rm90z@hotmail.com>
2026-05-16 01:45:17 -0500

    Sprint-2 commit

--- src/main/java/pe/edu/tecsup/lab03/entities/StudentEntity.java
+++ src/main/java/pe/edu/tecsup/lab03/entities/StudentEntity.java
@@ -1,4 +1,7 @@
 package pe.edu.tecsup.lab03.entities;
 
 public class StudentEntity {
+    void prueba (){
+
+    }
 }
--- src/main/java/pe/edu/tecsup/lab03/services/StudentService.java
+++ src/main/java/pe/edu/tecsup/lab03/services/StudentService.java
@@ -1,4 +1,7 @@
 package pe.edu.tecsup.lab03.services;
 
 public class StudentService {
+    void StudentService(){
+
+    }
 }
