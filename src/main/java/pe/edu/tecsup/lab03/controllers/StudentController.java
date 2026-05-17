73861879a0196696185dce06fb274098f6d1810b
L-Criss <linoc.joachin@gmail.com>
2026-05-17 00:26:12 -0500

    Mi primer commit de sptint-1

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -1,4 +1,8 @@
 package pe.edu.tecsup.lab03.controllers;
 
 public class StudentController {
+
+    public static void main(String[] args) {
+        System.out.println("Hola, he realizado un sprint-1");
+    }
 }
